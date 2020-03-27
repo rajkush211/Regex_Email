eb328c61dd24eb36766765851d9d1e538e4a4857
rajkush211 <rajkush211.rk@gmail.com>
2020-03-27 08:09:39 +0530

    ADDED: Validating mandatory part 1

---
+++ regexEmailAddress.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+#Description: Regex pattern to validate Email address
+#Author: Raj Kush
+#Date: 27 March 2020
+clear
+
+shopt -s extglob
+
+#CONSTANTS
+regexPattern="^\w+"
+
+#Variables
+emailAddress=0
+
+function validateEmailAddress() {
+	read -p "Enter Email address: " emailAddress
+	if [[ $emailAddress =~ $regexPattern ]]
+	then
+		echo "Valid"
+	else
+		echo "Invalid"
+	fi
+}
+
+validateEmailAddress
+
+sleep 1
